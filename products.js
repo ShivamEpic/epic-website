0f04856d89f0943ecffb9814ea8a377addc8ae32
ShivamEpic <kunjbiharit83@gmail.com>
2026-01-18 21:23:10 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,14 +1,12 @@
 const products = [
   {
-    name: "
-Clay Burst Booster Pack (Sealed) | Korean [sv2D]",
+    name: "Clay Burst Booster Pack (Sealed) | Korean [sv2D]",
     price: 170,
-    image: "images/sv-booster-pack.jpg",
+    image: "https://static.wixstatic.com/media/25c29c_91b0000b075348c4a340e1898297e7ed~mv2.png/v1/fill/w_560,h_560,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/25c29c_91b0000b075348c4a340e1898297e7ed~mv2.png",
     stock: true
   },
   {
-    name: "Booster Box – 
-InfernoX Booster Box (Sealed) | Korean [m2]
+    name: "Booster Box – InfernoX Booster Box (Sealed) | Korean [m2]
 ",
     price: 4599,
     image: "https://static.wixstatic.com/media/25c29c_10deb9c0868c4eb59734d91801be103e~mv2.png/v1/fill/w_420,h_420,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/25c29c_10deb9c0868c4eb59734d91801be103e~mv2.png",
