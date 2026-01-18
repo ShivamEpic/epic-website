548fb1ff276d8e710840cac5cbed571e611fa2ed
ShivamEpic <kunjbiharit83@gmail.com>
2026-01-18 19:53:47 +0530

    Create product.js

---
+++ product.js
@@ -0,0 +1,23 @@
+const products = [
+  {
+    name: "Scarlet & Violet SV1 Booster Pack",
+    price: 5140,
+    image: "https://www.google.com/search?q=scarlet+%26+violet+booster+pack+price&oq=scarlet+%26+violet+booster+pack+price&gs_lcrp=EgZjaHJvbWUyCQgAEEUYORiABDIICAEQABgWGB4yCAgCEAAYFhgeMggIAxAAGBYYHjIICAQQABgWGB4yCAgFEAAYFhgeMggIBhAAGBYYHjIICAcQABgWGB4yCAgIEAAYFhgeMggICRAAGBYYHjIICAoQABgWGB4yCAgLEAAYFhgeMggIDBAAGBYYHjIICA0QABgWGB4yCAgOEAAYFhge0gEJMjAwNTZqMGo3qAIUsAIB8QW-6HXdmGWlxw&client=ms-android-oppo-rvo3&sourceid=chrome-mobile&ie=UTF-8#piu=ps:45&sv=CAMS8gEatQESABosMmFoVUtFd2pIX1kyVnBKV1NBeFZvOERnR0hWeGZFZVlRZ2kxNkJBZ3dFQXciUgoUMTE2NTEwNzY0NzI1OTAzNjI3MjAaFDExODg4OTU3OTE2NjU4NjE3NjEzIhQxMTg4ODk1NzkxNjY1ODYxNzYxM0oCaGdSAKABE_ABAIICATAwAEItMmFoVUtFd2pIX1kyVnBKV1NBeFZvOERnR0hWeGZFZVlRcm9nR2VnUUlNQkFFIAYqMgoOcHZlLVNUUkVBTV9QSVUSHnB2ZV81LXRzYVlmdU4tamc0LUVQM0w3RnNBNF8xOBgBMAE4ASDJrfvjAjABSgoIARACGAEgASgB.jpg",
+    type: "pack",
+    stock: true
+  },
+  {
+    name: "Booster Box – Scarlet & Violet",
+    price: 4999,
+    image: "https://m.media-amazon.com/images/I/81WDYOQ-MZL._SX679_.jpg",
+    type: "box",
+    stock: true
+  },
+  {
+    name: "Rare Pokémon Card",
+    price: 120,
+    image: "https://assets.pokemon.com/assets/cms2/img/cards/web/SV01/SV01_EN_1.png",
+    type: "card",
+    stock: true
+  }
+];
