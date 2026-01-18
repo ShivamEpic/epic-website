9f7d0cf2919468e2c908b68567c85dc8cdd6e5c3
ShivamEpic <kunjbiharit83@gmail.com>
2026-01-18 20:03:19 +0530

    Update and rename product.js to products.js

---
+++ products.js
@@ -0,0 +1,23 @@
+const products = [
+  {
+    name: "Scarlet & Violet SV1 Booster Pack",
+    price: 399,
+    image: "https://m.media-amazon.com/images/I/81C1bP6Z4RL._SX679_.jpg",
+    type: "pack",
+    stock: true
+  },
+  {
+    name: "Booster Box – Scarlet & Violet",
+    price: 4999,
+    image: "https://m.media-amazon.com/images/I/81WDYOQ-MZL._SX679_.jpg",
+    type: "box",
+    stock: true
+  },
+  {
+    name: "Rare Pokémon Card",
+    price: 499,
+    image: "https://assets.pokemon.com/assets/cms2/img/cards/web/SV01/SV01_EN_1.png",
+    type: "card",
+    stock: true
+  }
+];
