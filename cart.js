abe81184088ec467fb845eda9089381d5ac95121
ShivamEpic <kunjbiharit83@gmail.com>
2026-02-08 11:05:55 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,6 +1,7 @@
 const cartItems = document.getElementById("cartItems");
 const totalEl = document.getElementById("total");
 
+// Load cart from localStorage
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 function renderCart() {
@@ -14,20 +15,25 @@ function renderCart() {
   }
 
   cart.forEach((item, index) => {
-    total += Number(item.price) * Number(item.qty);
+    const price = Number(item.price) || 0;
+    const qty = Number(item.qty) || 1;
+    total += price * qty;
 
     const div = document.createElement("div");
-    div.className = "item";
+    div.className = "cart-item";
 
     div.innerHTML = `
-      <img src="images/${item.image}">
-      <div>
-        <b>${item.name}</b><br>
-        ₹${item.price} × ${item.qty}
-        <br>
-        <button onclick="changeQty(${index}, -1)">−</button>
-        <button onclick="changeQty(${index}, 1)">+</button>
-        <button onclick="removeItem(${index})">Remove</button>
+      <img src="${item.image}" alt="${item.name}">
+      <div class="cart-info">
+        <h4>${item.name}</h4>
+        <p>₹${price} × ${qty}</p>
+
+        <div class="cart-actions">
+          <button onclick="changeQty(${index}, -1)">−</button>
+          <span>${qty}</span>
+          <button onclick="changeQty(${index}, 1)">+</button>
+          <button onclick="removeItem(${index})">Remove</button>
+        </div>
       </div>
     `;
 
@@ -38,15 +44,22 @@ function renderCart() {
   localStorage.setItem("cart", JSON.stringify(cart));
 }
 
+// Change quantity
 window.changeQty = function (index, change) {
-  cart[index].qty += change;
-  if (cart[index].qty <= 0) cart.splice(index, 1);
+  cart[index].qty = (Number(cart[index].qty) || 1) + change;
+
+  if (cart[index].qty <= 0) {
+    cart.splice(index, 1);
+  }
+
   renderCart();
 };
 
+// Remove item
 window.removeItem = function (index) {
   cart.splice(index, 1);
   renderCart();
 };
 
+// Initial render
 renderCart();
